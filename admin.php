3ef6d7aeae9da4174d970ad6706c59528a663846
Autumn Clara Watt <awatt@dhcp-rhodes-3257.eduroam.cornell.edu>
2018-04-20 15:50:00 -0400

    added pages

---
+++ admin.php
@@ -0,0 +1,16 @@
+<?php include('includes/init.php');
+$current_page_id = "admin"; ?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+  <?php include('includes/head.php'); ?>
+  <title>Admin</title>
+</head>
+
+<body>
+  <?php include("includes/header.php"); ?>
+
+</body>
+</html>
---
+++ signchoir.php
@@ -0,0 +1,16 @@
+<?php include('includes/init.php');
+$current_page_id = "signchoir"; ?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+  <?php include('includes/head.php'); ?>
+  <title>Sign Choir</title>
+</head>
+
+<body>
+  <?php include("includes/header.php"); ?>
+
+</body>
+</html>
