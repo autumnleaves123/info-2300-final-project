fe8360e9ab14d3f177ea2daa0fc778f569402250
Autumn Clara Watt <awatt@dhcp-rhodes-3257.eduroam.cornell.edu>
2018-04-20 15:28:14 -0400

    made webpages

---
+++ events.php
@@ -0,0 +1,17 @@
+<?php include('includes/init.php');
+$current_page_id = "events"; ?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+  <title>Events</title>
+</head>
+
+<body>
+
+</body>
+</html>
---
+++ signs.php
@@ -0,0 +1,17 @@
+<?php include('includes/init.php');
+$current_page_id = "login"; ?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+  <title>Login</title>
+</head>
+
+<body>
+
+</body>
+</html>
