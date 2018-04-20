472f0cf86310f5be53fa55466c7ff0c3224b9489
Autumn Clara Watt <awatt@dhcp-rhodes-3257.eduroam.cornell.edu>
2018-04-20 15:22:22 -0400

    hi

--- meettheboard.php
+++ meettheboard.php
@@ -1,5 +1,5 @@
 <?php include('includes/init.php');
-$current_page_id = "home"; ?>
+$current_page_id = "meettheboard"; ?>
 
 <!DOCTYPE html>
 <html>
@@ -8,7 +8,7 @@ $current_page_id = "home"; ?>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-  <title>Home</title>
+  <title>Meet the Board</title>
 </head>
 
 <body>
