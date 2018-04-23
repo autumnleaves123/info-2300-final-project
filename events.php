410565d31c316eb3e651ac34930d8d5b9bdfc4fb
Bethany Tang <bethanytang@dhcp-ccc-1418.eduroam.cornell.edu>
2018-04-23 13:30:34 -0400

    added events table and signchoir info

--- events.php
+++ events.php
@@ -14,8 +14,35 @@ $current_page_id = "events"; ?>
   <section>
 		<h1>Events</h1>
 
-		<!-- TODO: Create table with events -->
-		<!-- Table: Date, time, event, location -->
+		<table>
+			<tr>
+				<th>Date</th>
+				<th>Time</th>
+				<th>Location</th>
+				<th>Event</th>
+			</tr>
+
+			<tr>
+				<td>April 17 2018</td>
+				<td>4:30PM - 6:00PM</td>
+				<td>Ives Hall Room 116</td>
+				<td>Undergraduate Disability Studies Journal Panel</td>
+			</tr>
+
+			<tr>
+				<td>April 18 2018</td>
+				<td>5:30PM - 6:30PM</td>
+				<td>Goldwin Smith Hall Room 164</td>
+				<td>Sign Choir Meeting</td>
+			</tr>
+
+			<tr>
+				<td>April 24 2018</td>
+				<td>9:00PM</td>
+				<td>Balch Hall</td>
+				<td>CUDAP Arch Sign</td>
+			</tr>
+		</table>
 
   </section>
 
--- signchoir.php
+++ signchoir.php
@@ -19,7 +19,7 @@ $current_page_id = "signchoir"; ?>
 
 		<p>Over the past several years, we have been holding weekly sign choir workshops during which we teach student-interpreted ASL translations of popular songs. We have also performed in various venues. Our current repertoire includes “It's Time" by Imagine Dragons, "Fight Song" by Rachel Platten, "Love Yourself" by Justin Bieber, and "Sunday Mornings" by Maroon 5.</p>
 
-		<p>You can find upcoming Sign Choir rehearsals and performances on our events page.</p>
+		<p>You can find upcoming Sign Choir rehearsals and performances on our <a href='events.php'>events</a> page.</p>
   </section>
 
   <?php include('includes/footer.php'); ?>
