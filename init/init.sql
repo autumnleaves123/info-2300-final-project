1091f5278361a4519a31a5c1c3661bbd42d46a96
Hyun Seo (Lucy) Lee <hl566@cornell.edu>
2018-04-25 09:56:23 -0400

    p4 m3

--- init/init.sql
+++ init/init.sql
@@ -1,4 +1,23 @@
 /* TODO: create tables */
 
+/* Home page (mewsfeed) */
+
+/* Meet the board (board members) */
+
+/* Gallery */
+
+/* Learn ASL */
+
+/* Resources */
 
 /* TODO: initial seed data */
+
+/* Home page (mewsfeed) */
+
+/* Meet the board (board members) */
+
+/* Gallery */
+
+/* Learn ASL */
+
+/* Resources */
