619a17f7df655768ef1ee085c4209853c635df05
Derek Cheng <dsc252@cornell.edu>
2018-04-25 00:27:14 -0400

    remove head.php

--- login.php
+++ login.php
@@ -10,7 +10,10 @@ $current_page_id = "login";
 <html lang="en">
 
 <head>
-  <?php include('includes/head.php'); ?>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <link rel="stylesheet" type="text/css" href="styles/all.css" media="all" />
+
   <title>Login</title>
 </head>
 
