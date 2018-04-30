d9ae4fef575f1cde57a26543bc05e86e62c9bf8b
Hyun Seo (Lucy) Lee <hl566@cornell.edu>
2018-04-30 15:39:52 -0400

    p4 m3 resources page

--- resources.php
+++ resources.php
@@ -16,7 +16,16 @@ $current_page_id = "learn"; ?>
   <?php include("includes/header.php"); ?>
   <section class = "content">
 		<h1>Resources</h1>
-
+    <p>See the links below to learn ASL:
+      <div class = "indent"><a href="https://www.startasl.com/learn-sign-language-asl" target="_blank">www.startasl.com</a><br/></div>
+      <div class = "indent"><a href="http://www.signlanguage101.com/" target="_blank">www.signlanguage101.com</a></div> <br/>
+    </p>
+    <p>Click on the links below to download our learning ASL Powerpoints:
+      <div class="indent"><a href="uploads/resourcesppt/animal.pptx">Animal Vocab</a><br/></div>
+      <div class="indent"><a href="uploads/resourcesppt/education.pptx">Education Vocab</a><br/></div>
+      <div class="indent"><a href="uploads/resourcesppt/emotions.pptx">Emotions Vocab</a><br/></div>
+      <div class="indent"><a href="uploads/resourcesppt/health.pptx">Health Vocab</a><br/></div>
+    </p>
 
   </section>
 
