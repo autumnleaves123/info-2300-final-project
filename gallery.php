35089e30bb6b359d7fcecd37504abd4a4a19c005
Derek Cheng <dsc252@cornell.edu>
2018-05-01 04:07:22 -0400

    partially implement gallery page

--- gallery.php
+++ gallery.php
@@ -27,6 +27,43 @@ $current_page_id = "gallery";
 
 		<!-- TODO: Print images from gallery/images array -->
 
+    <div class="row">
+      <div class="column">
+        <img src="/uploads/images/wedding.jpg" style="width:100%">
+        <img src="/uploads/images/rocks.jpg" style="width:100%">
+        <img src="/uploads/images/falls2.jpg" style="width:100%">
+        <img src="/uploads/images/paris.jpg" style="width:100%">
+        <img src="/uploads/images/nature.jpg" style="width:100%">
+        <img src="/uploads/images/mist.jpg" style="width:100%">
+        <img src="/uploads/images/paris.jpg" style="width:100%">
+      </div>
+      <div class="column">
+        <img src="/uploads/images/underwater.jpg" style="width:100%">
+        <img src="/uploads/images/ocean.jpg" style="width:100%">
+        <img src="/uploads/images/wedding.jpg" style="width:100%">
+        <img src="/uploads/images/mountainskies.jpg" style="width:100%">
+        <img src="/uploads/images/rocks.jpg" style="width:100%">
+        <img src="/uploads/images/underwater.jpg" style="width:100%">
+      </div>
+      <div class="column">
+        <img src="/uploads/images/wedding.jpg" style="width:100%">
+        <img src="/uploads/images/rocks.jpg" style="width:100%">
+        <img src="/uploads/images/falls2.jpg" style="width:100%">
+        <img src="/uploads/images/paris.jpg" style="width:100%">
+        <img src="/uploads/images/nature.jpg" style="width:100%">
+        <img src="/uploads/images/mist.jpg" style="width:100%">
+        <img src="/uploads/images/paris.jpg" style="width:100%">
+      </div>
+      <div class="column">
+        <img src="/uploads/images/underwater.jpg" style="width:100%">
+        <img src="/uploads/images/ocean.jpg" style="width:100%">
+        <img src="/uploads/images/wedding.jpg" style="width:100%">
+        <img src="/uploads/images/mountainskies.jpg" style="width:100%">
+        <img src="/uploads/images/rocks.jpg" style="width:100%">
+        <img src="/uploads/images/underwater.jpg" style="width:100%">
+      </div>
+    </div>
+
 
   </section>
 
