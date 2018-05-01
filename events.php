ae73f150f7626e2cc4d558d28dedb651e7a934ea
Bethany Tang <bethanytang@dhcp-ccc-3121.eduroam.cornell.edu>
2018-05-01 11:58:41 -0400

    content and white-background classes

--- events.php
+++ events.php
@@ -14,39 +14,40 @@ $current_page_id = "events"; ?>
 
 <body>
   <?php include("includes/header.php"); ?>
-  <section class="content">
+  <section class="content2">
 		<h1>Events</h1>
 
-		<table>
-			<tr>
-				<th>Date</th>
-				<th>Time</th>
-				<th>Location</th>
-				<th>Event</th>
-			</tr>
+		<div class="white-background">
+			<table>
+				<tr>
+					<th>Date</th>
+					<th>Time</th>
+					<th>Location</th>
+					<th>Event</th>
+				</tr>
 
-			<tr>
-				<td>April 17 2018</td>
-				<td>4:30PM - 6:00PM</td>
-				<td>Ives Hall Room 116</td>
-				<td>Undergraduate Disability Studies Journal Panel</td>
-			</tr>
+				<tr>
+					<td>April 17 2018</td>
+					<td>4:30PM - 6:00PM</td>
+					<td>Ives Hall Room 116</td>
+					<td>Undergraduate Disability Studies Journal Panel</td>
+				</tr>
 
-			<tr>
-				<td>April 18 2018</td>
-				<td>5:30PM - 6:30PM</td>
-				<td>Goldwin Smith Hall Room 164</td>
-				<td>Sign Choir Meeting</td>
-			</tr>
+				<tr>
+					<td>April 18 2018</td>
+					<td>5:30PM - 6:30PM</td>
+					<td>Goldwin Smith Hall Room 164</td>
+					<td>Sign Choir Meeting</td>
+				</tr>
 
-			<tr>
-				<td>April 24 2018</td>
-				<td>9:00PM</td>
-				<td>Balch Hall</td>
-				<td>CUDAP Arch Sign</td>
-			</tr>
-		</table>
-
+				<tr>
+					<td>April 24 2018</td>
+					<td>9:00PM</td>
+					<td>Balch Hall</td>
+					<td>CUDAP Arch Sign</td>
+				</tr>
+			</table>
+		</div>
   </section>
 
   <?php include('includes/footer.php'); ?>
