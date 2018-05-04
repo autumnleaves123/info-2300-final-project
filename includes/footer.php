d050d7c691ee98f33aebce377dc7788cd90c1672
Bethany Tang <bethanytang@dhcp-hol-3585.eduroam.cornell.edu>
2018-05-04 13:28:18 -0400

    admin changes

--- includes/footer.php
+++ includes/footer.php
@@ -2,10 +2,12 @@
 		<div id="footer-left">
 			<!-- no content -->
 		</div>
+
 		<div id="footer-center">
 			<p>Cornell University Deaf Awareness Project. 2018-2019.
 			<p>Designed by Autumn Watt, Bethany Tang, Derek Cheng, Lucy Lee.</p>
 		</div>
+
 		<div id="footer-right">
 			<!-- Source: https://en.facebookbrand.com/assets/f-logo -->
 			<a href="https://www.facebook.com/cudeafawarenessproject/" target="_blank"><img alt="Facebook Logo" src="images/facebook.png"/></a>
