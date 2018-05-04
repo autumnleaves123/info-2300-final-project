0d0a1825c909eed8c9c7bd80c2182b84fd99c36a
Bethany Tang <bethanytang@dhcp-rhodes-2809.eduroam.cornell.edu>
2018-05-04 15:33:50 -0400

    feed tag search works!

--- index.php
+++ index.php
@@ -6,17 +6,28 @@ error_reporting(E_ALL);
 include('includes/init.php');
 $current_page_id = "index";
 
-// fetch feed content
-$sql = "SELECT * FROM feed";
-$params = array();
-$fetch_feed_content = exec_sql_query($db, $sql, $params)->fetchAll();
-
-// fetch tags
+// fetch all tags
 $sql = "SELECT * FROM feed_tags";
 $params = array();
 $fetch_feed_tags = exec_sql_query($db, $sql, $params)->fetchAll();
 
-// fetch feed content with tags
+// if user clicks on a tag
+if ( isset($_GET["tag"])) {
+	$current_tag = filter_input(INPUT_GET, 'tag', FILTER_SANITIZE_STRING);
+
+	// fetch images based on TAGS
+	$sql = "SELECT * FROM feed_to_tags INNER JOIN feed ON feed_to_tags.feed_id = feed.id WHERE tag_id = :current_tag";
+	$params = array(':current_tag' => $current_tag);
+	$fetch_feed_content = exec_sql_query($db, $sql, $params)->fetchAll();
+
+} else {
+	$current_tag = NULL;
+
+	// fetch feed content
+	$sql = "SELECT * FROM feed";
+	$params = array();
+	$fetch_feed_content = exec_sql_query($db, $sql, $params)->fetchAll();
+}
 
 
 ?>
@@ -33,14 +44,14 @@ $fetch_feed_tags = exec_sql_query($db, $sql, $params)->fetchAll();
 </head>
 
 <body>
-	<div id="fb-root"></div>
+	<!-- <div id="fb-root"></div>
 	<script>(function(d, s, id) {
   var js, fjs = d.getElementsByTagName(s)[0];
   if (d.getElementById(id)) return;
   js = d.createElement(s); js.id = id;
   js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.0';
   fjs.parentNode.insertBefore(js, fjs);
-	}(document, 'script', 'facebook-jssdk'));</script>
+	}(document, 'script', 'facebook-jssdk'));</script> -->
 
 	<?php include('includes/header.php'); ?>
 
@@ -70,7 +81,21 @@ $fetch_feed_tags = exec_sql_query($db, $sql, $params)->fetchAll();
 						<div class="post">
 							<h2><?php echo "$post[title]";?></h2>
 							<p><?php echo "$post[entry_date]";?></p>
+
+							<?php
+							$post_id = $post['id'];
+
+							$sql = "SELECT feed_id, tag_id, name FROM feed_to_tags INNER JOIN feed_tags ON feed_to_tags.tag_id = feed_tags.id WHERE feed_id = :post_id";
+							$params = array(':post_id' => $post_id);
+							$feed_tags = exec_sql_query($db, $sql, $params)->fetchAll();
+							?>
+
+							<ul id="tags-in-post">
+								<?php foreach ($feed_tags as $tag) { echo "<li>" .$tag['name'] ."</li>"; } ?>
+							</ul>
+
 							<p><?php echo "$post[content]";?></p>
+
 						</div>
 				<?php } ?>
 			</div>
