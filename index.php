328c49791e08bf46b560911c47c7b1b9debbd10a
Bethany Tang <bethanytang@Bethanys-MacBook-Pro.local>
2018-05-08 22:25:09 -0400

    search criteria cross

--- index.php
+++ index.php
@@ -45,15 +45,6 @@ if ( isset($_GET["tag"])) {
 </head>
 
 <body>
-	<!-- <div id="fb-root"></div>
-	<script>(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.0';
-  fjs.parentNode.insertBefore(js, fjs);
-	}(document, 'script', 'facebook-jssdk'));</script> -->
-
 	<?php include('includes/header.php'); ?>
 
 	<section id="index">
