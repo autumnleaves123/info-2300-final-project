679adc0bdf78201333036a27502b0e7068d85ca6
Autumn Clara Watt <awatt@dhcp-gs-7446.eduroam.cornell.edu>
2018-05-14 00:23:01 -0400

    more changes

--- admin-gallery.php
+++ admin-gallery.php
@@ -7,6 +7,76 @@ if ($current_user == NULL) {
 	header("Location: login.php");
 }
 
+/*
+// upload ppt form processing
+if (isset($_POST['add-ppt-button'])) {
+	$upload_info = $_FILES["ppt-file"];
+
+	$label = $_POST['ppt-label'];
+	$label = strtolower(trim(filter_var($label, FILTER_SANITIZE_STRING)));
+
+	if ($upload_info['error'] == UPLOAD_ERR_OK) {
+		$target_file = basename($_FILES["ppt-file"]["name"]);
+		$filetype = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+		if($filetype != "ppt" & $filetype != "pptx") {
+			array_push($messages, "[Error uploading your file.]");
+			array_push($messages, "[Wrong file type. Only PPT & PPTX files are allowed.]");
+		} else {
+			// check if file already exists because must be UNIQUE
+			$sql = "SELECT * FROM ppts WHERE label = :label;";
+			$params = array(":label"=>$label);
+			$records = exec_sql_query($db, $sql, $params)->FetchAll();
+
+			// if $records not empty, file already exists
+			if ($records) {
+				array_push($messages, "[File already exists.]");
+			} else {
+				var_dump("in else");
+				// if $records empty, file does not exist, so upload it
+				$sql = "INSERT INTO ppts (file, label) VALUES (:target_file, :label)";
+				$params = array(':target_file' => $target_file, ':label' => $label);
+				$records = exec_sql_query($db, $sql, $params);
+
+				$fileid = $db->lastInsertId("id");
+				$newfilename = "$fileid.$filetype";
+				$destination = FILE_UPLOADS_PATH . $newfilename;
+				$sql = "UPDATE ppts SET file = :newfilename WHERE label = :label";
+				$params = array(':newfilename' => $newfilename, ':label' => $label);
+				$records = exec_sql_query($db, $sql, $params);
+				if (move_uploaded_file($upload_info["tmp_name"], $destination)) {
+					array_push($messages, "[The new powerpoint '". htmlspecialchars($label) . "' has been added.]");
+				} else {
+					array_push($messages, "[Error uploading your powerpoint.]");
+				}
+			}
+		}
+	} else {
+		array_push($messages, "[Error uploading your powerpoint.]");
+		if ($upload_info['error'] == UPLOAD_ERR_FORM_SIZE) {
+			array_push($messages, "[File size too large.]");
+		}
+	}
+}
+
+// delete powerpoint file form processing
+if (isset($_POST['delete-ppt-button'])) {
+  $ppt_delete = $_POST['ppt-names'];
+	$ppt_delete = trim(filter_var($ppt_delete, FILTER_SANITIZE_STRING));
+	var_dump($ppt_delete);
+	$sqlfile = "SELECT file FROM ppts WHERE label = :ppt_delete";
+  $paramsfile = array(':ppt_delete' => $ppt_delete);
+  $file_delete = exec_sql_query($db, $sqlfile, $paramsfile)->fetchAll();
+	var_dump($file_delete);
+  $locationoffile = FILE_UPLOADS_PATH . $file_delete[0]['file'];
+  $sql = "DELETE FROM ppts WHERE label = :ppt_delete";
+  $params = array(':ppt_delete' => $ppt_delete);
+  $records = exec_sql_query($db, $sql, $params);
+
+  unlink($locationoffile);
+  array_push($messages, "[The powerpoint (". ucwords(htmlspecialchars($ppt_delete)) . ") has been successfully deleted.]");
+}
+*/
+
 
 ?>
 
