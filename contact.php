e8bae1b32368cdb1485c0cf094ac8e9e3ed0372a
Bethany Tang <bethanytang@Bethanys-MacBook-Pro.local>
2018-05-15 11:45:21 -0400

    update contact form

--- contact.php
+++ contact.php
@@ -5,20 +5,19 @@ $current_page_id = "contact";
 // when the user submits a form
 if (isset($_POST["submit"])) {
 		// show success message
-	$email = $_POST['email'];
+	$email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
 	record_message("You have been subscribed to the mailing list.");
-  // mail("cudap-l-request@cornell.edu", "join", "", "From: " . $email);
-	$send = mail("bt293@cornell.edu", "join", "", "From: " . $email);
-	var_dump($send);
+  mail("cudap-l-request@cornell.edu", "join", "", "From: " . $email);
+	// to TAs: you can replace cudap-l-request@cornell.edu with your personal email in the mail() function to test that the form works
 }
 
 // when the user submits a form
 if (isset($_POST["submitmessage"])) {
 	// validate form here
-	$message = $_POST['message'];
+	$message = filter_input(INPUT_POST, 'message', FILTER_SANITIZE_STRING);
   record_message("Your message has been received.");
-	mail("cudap-l-request@cornell.edu", "message", $message, "");
-	mail("bt293@cornell.edu", "message", $message, "");
+	mail("cudap-l-request@cornell.edu", "Message From Website", $message, "");
+	// to TAs: you can replace cudap-l-request@cornell.edu with your personal email in the mail() function to test that the form works
 }
 
 ?>
