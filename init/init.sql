b69fe267c815e5e6975bbe5844198e26177d972d
Bethany Tang <bethanytang@Bethanys-MacBook-Pro.local>
2018-05-15 14:07:11 -0400

    fix tags

--- init/init.sql
+++ init/init.sql
@@ -110,13 +110,13 @@ INSERT INTO feed_tags (name) VALUES ("#interest");
 INSERT INTO feed_tags (name) VALUES ("#apparel");
 INSERT INTO feed_tags (name) VALUES ("#officehours");
 
-INSERT INTO feed_to_tags (feed_id, tag_id) VALUES (1, 2);
-INSERT INTO feed_to_tags (feed_id, tag_id) VALUES (2, 1);
-INSERT INTO feed_to_tags (feed_id, tag_id) VALUES (3, 4);
+INSERT INTO feed_to_tags (feed_id, tag_id) VALUES (1, 4);
+INSERT INTO feed_to_tags (feed_id, tag_id) VALUES (2, 6);
+INSERT INTO feed_to_tags (feed_id, tag_id) VALUES (3, 5);
 INSERT INTO feed_to_tags (feed_id, tag_id) VALUES (4, 3);
-INSERT INTO feed_to_tags (feed_id, tag_id) VALUES (5, 5);
-INSERT INTO feed_to_tags (feed_id, tag_id) VALUES (6, 6);
-INSERT INTO feed_to_tags (feed_id, tag_id) VALUES (7, 4);
+INSERT INTO feed_to_tags (feed_id, tag_id) VALUES (5, 4);
+INSERT INTO feed_to_tags (feed_id, tag_id) VALUES (6, 1);
+INSERT INTO feed_to_tags (feed_id, tag_id) VALUES (7, 2);
 
 INSERT INTO feed_attachments (file_name, file_ext) VALUES ("officehourslist.docx", "docx");
 INSERT INTO feed_attachments (file_name, file_ext) VALUES ("board-descriptions.pdf", "pdf");
