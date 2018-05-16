a737017ae13f76853c3829b84eb999f6ba48497f
Autumn Clara Watt <awatt@dhcp-hol-1388.redrover.cornell.edu>
2018-05-16 09:09:47 -0400

    hi

--- contact.php
+++ contact.php
@@ -27,13 +27,8 @@ if (isset($_POST["submitmessage"])) {
 <html>
 
 <head>
-	<meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1" />
-  <link rel="stylesheet" type="text/css" href="styles/all.css" media="all" />
-	<link rel="stylesheet" type="text/css" href="styles/tablet.css"/>
-	<link rel="stylesheet" type="text/css" href="styles/mobile.css"/>
-
-  <title>Contact</title>
+	<?php include('includes/head.php'); ?>
+	<title>Contact</title>
 
 </head>
 
@@ -44,10 +39,8 @@ if (isset($_POST["submitmessage"])) {
 		<h1>Contact Us</h1>
 
 		<div class="white-background no-padding">
-
 			<div id="contactCentering">
 				<?php print_messages();?>
-
 				<div class="contactpage">
 					<h2>Questions or Feedback?</h2>
 					<p>Contact our officers at <span class="highlight"><a href="mailto:cudap@cornell.edu" target="_blank">cudap@cornell.edu</a></span>, or send us a message on <span class="highlight"><a class="highlight" href="https://www.facebook.com/cudeafawarenessproject/" target="_blank">Facebook</a></span>.<br/>
@@ -61,8 +54,8 @@ if (isset($_POST["submitmessage"])) {
 							<h2>Mailing List</h2>
 							<p>Leave us your email for information and updates!</p>
 							<form method="post" action="contact.php" id="joinForm">
-									<input name="email" type="email" placeholder="netid@cornell.edu" required>
-									<button name="submit" type="submit" class="submit">subscribe to listserv</button>
+								<input name="email" type="email" placeholder="netid@cornell.edu" required>
+								<button name="submit" type="submit" class="submit">subscribe to listserv</button>
 							</form>
 						</div>
 
@@ -71,18 +64,16 @@ if (isset($_POST["submitmessage"])) {
 							<h2>Send Us A Message</h2>
 							<p>Please leave any inquiries, comments, and/or feedback you might have.</p>
 							<form method="post" action="contact.php" id="messageForm" >
-									<textarea rows="7" cols="40" name="message" placeholder="Write your message here" required></textarea>
-									<button type="submit" name="submitmessage">submit message</button>
+								<textarea rows="7" cols="40" name="message" placeholder="Write your message here" required></textarea>
+								<button type="submit" name="submitmessage">submit message</button>
 							</form>
 						</div>
 
 					</div>
 				</div>
 			</div>
-
 		</section>
-
 		<?php include('includes/footer.php'); ?>
-
+		
 	</body>
 </html>
